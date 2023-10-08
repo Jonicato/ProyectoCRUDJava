7d68e6ba42f4903e1265fd97511aca2e74213d61
Jonicato <iam.jonicato@gmail.com>
2023-10-08 11:41:51 -0600

    first commit

---
+++ src/main/java/org/example/platzi/repository/Repository.java
@@ -0,0 +1,14 @@
+package org.example.platzi.repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Repository<T> {
+    List<T> findAll() throws SQLException;
+
+    T getById(Integer id) throws SQLException;
+    void save(T t);
+
+    void delete(Integer id);
+
+}

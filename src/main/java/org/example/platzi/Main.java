0bdf244f49d3587acb80750982bf6893734bea47
Jonicato <iam.jonicato@gmail.com>
2023-10-09 17:30:26 -0600

    CRUD terminado e interfaz básica

--- src/main/java/org/example/platzi/Main.java
+++ src/main/java/org/example/platzi/Main.java
@@ -1,22 +1,14 @@
 package org.example.platzi;
 
-import org.example.platzi.model.Employee;
-import org.example.platzi.repository.EmployeeRepository;
-import org.example.platzi.repository.Repository;
-import org.example.platzi.util.DatabaseConnection;
+import org.example.platzi.view.SwingApp;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
-
-        try(Connection myConn = DatabaseConnection.getInstance()) {
-            Repository<Employee> repository = new EmployeeRepository();
-
-            repository.findAll().forEach(System.out::println);
-        }
+        SwingApp app =  new SwingApp();
+        app.setVisible(true);
     }
 }

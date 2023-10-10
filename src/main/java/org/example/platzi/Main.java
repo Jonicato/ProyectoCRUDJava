c51efa3a16485ef380a1d03bab70f08956686120
Jonicato <iam.jonicato@gmail.com>
2023-10-10 13:33:44 -0600

    Implementando pool de conexiones

--- src/main/java/org/example/platzi/Main.java
+++ src/main/java/org/example/platzi/Main.java
@@ -3,46 +3,18 @@ package org.example.platzi;
 import org.example.platzi.model.Employee;
 import org.example.platzi.repository.EmployeeRepository;
 import org.example.platzi.repository.Repository;
-import org.example.platzi.util.DatabaseConnection;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try (Connection myConn = DatabaseConnection.getInstance()) {
-            if (myConn.getAutoCommit()) {
-                myConn.setAutoCommit(false);
-            }
+        System.out.println("----Listando todos----");
+        Repository<Employee> repository = new EmployeeRepository();
+        repository.findAll().forEach(System.out::println);
 
-            try {
-                Repository<Employee> repository = new EmployeeRepository(myConn);
-
-                System.out.println("----Insertar un nuevo empleado----");
-                Employee employee = new Employee();
-                employee.setFirst_name("Miguel");
-                employee.setPa_surname("Garcia");
-                employee.setMa_surname("Tapia");
-                employee.setEmail("migue@example.com");
-                employee.setSalary(30000F);
-                employee.setCurp("MIGT090909MGHTCZ08");
-                repository.save(employee);
-
-                employee.setFirst_name("David");
-                employee.setPa_surname("Gutiérrez");
-                employee.setMa_surname("Olvera");
-                employee.setEmail("olv@example.com");
-                employee.setSalary(3000F);
-                employee.setCurp("AMEC2345678YLDGM06");
-                repository.save(employee);
-                myConn.commit();
-
-            } catch (SQLException e) {
-                myConn.rollback();
-                throw new RuntimeException(e);
-            }
-        }
+        System.out.println("----Buscando por ID----");
+        System.out.println(repository.getById(4));
     }
 }
--- src/main/java/org/example/platzi/util/DatabaseConnection.java
+++ src/main/java/org/example/platzi/util/DatabaseConnection.java
@@ -1,19 +1,33 @@
 package org.example.platzi.util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static String url = "jdbc:mysql://localhost:3306/project";
     private static String user = "root";
     private static String password = "";
-    private static Connection myConn;
+    private static BasicDataSource pool;
 
-    public static Connection getInstance() throws SQLException {
-        if (myConn == null) {
-            myConn = DriverManager.getConnection(url, user, password);
+    public static BasicDataSource getInstance() throws SQLException {
+        if (pool == null) {
+            pool = new BasicDataSource();
+            pool.setUrl(url);
+            pool.setUsername(user);
+            pool.setPassword(password);
+
+            pool.setInitialSize(3);
+            pool.setMinIdle(3);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
         }
-        return myConn;
+        return pool;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        // Obtener una sola conexión del pool
+        return getInstance().getConnection();
     }
 }
